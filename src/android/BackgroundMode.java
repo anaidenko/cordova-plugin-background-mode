2b19b7d0dead6dc43b6bc5e97b90606cb35d2d66
Andrii Naidenko <Andrii.Naidenko@outlook.com>
2023-09-12 18:08:09 +0200

    Fix random "Service not registered" error when trying to stop service

--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -263,9 +263,13 @@ public class BackgroundMode extends CordovaPlugin {
 
         if (!isBind) return;
 
-        fireEvent(Event.DEACTIVATE, null);
-        context.unbindService(connection);
-        context.stopService(intent);
+        try {
+            fireEvent(Event.DEACTIVATE, null);
+            context.unbindService(connection);
+            context.stopService(intent);
+        } catch (Exception e) {
+            fireEvent(Event.FAILURE, String.format("'%s'", e.getMessage()));
+        }
 
         isBind = false;
     }
